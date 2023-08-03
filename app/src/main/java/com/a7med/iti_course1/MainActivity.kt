1a36b88a7e87e04b9cfbb883d846b661ef2598c2
AhmedTarekAli <att127emd@gmail.com>
2023-08-03 15:57:57 +0300

    new branch

--- app/src/main/java/com/a7med/iti_course1/MainActivity.kt
+++ app/src/main/java/com/a7med/iti_course1/MainActivity.kt
@@ -2,14 +2,27 @@ package com.a7med.iti_course1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.a7med.iti_course1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
-
+lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
      binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.login.setOnClickListener {
+
+           Toast.makeText(this, "User name : ${binding.editTtext.text}", Toast.LENGTH_SHORT).show()
+           Toast.makeText(this, "Password : ${binding.passwrdEd.text}", Toast.LENGTH_SHORT).show()
+          //  Toast.makeText(this, "User name = ${binding.radio.}", Toast.LENGTH_SHORT).show()
+
+           // Toast.makeText(this, "User3 name = ${binding.liner1}", Toast.LENGTH_SHORT).show()
+
+
+        }
+
 
     }
 }
